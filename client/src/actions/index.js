29006314b206814a27b60bbed3ee8482371f5b29
HenryTanjaya <henrytanjaya11@gmail.com>
2018-03-01 16:42:02 +0700

    change url

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {reset} from 'redux-form';
 import {browserHistory} from 'react-router';
 import {AUTH_USER,AUTH_ERROR,UNAUTH_USER,FETCH_LANDING,UPDATE_LANDING,FETCH_PORTFOLIO_INDEX,FETCH_PORTFOLIO_SHOW,UPDATE_PORTFOLIO,NEW_PORTFOLIO,REMOVE_PORTFOLIO,SEND_FORM} from './types';
-var ROOT_URL='https://simpulweds.herokuapp.com';
+var ROOT_URL='http://simpulweds.com';
 const hostname = window && window.location && window.location.hostname;
 if(hostname==="localhost"){
   ROOT_URL='http://localhost:3090'
