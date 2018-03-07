043a745d72a406772afa3f6d23ac80c25aa49545
unknown <henrytanjaya11@gmail.com>
2018-03-07 11:04:27 +0700

    add link back to memories

--- client/src/components/portfolio/portfolio_show.js
+++ client/src/components/portfolio/portfolio_show.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router';
 import * as actions from '../../actions';
 
 class PortfolioShow extends Component {
