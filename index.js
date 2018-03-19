40493029ac185fe2c4b9d23ac1a5d9f1eea3b92a
unknown <henrytanjaya11@gmail.com>
2018-03-19 13:09:58 +0700

    plugin

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ app.use('/api/portfolio/', portfolioRoutes);
 
 if(process.env.NODE_ENV==='production'){
   app.use(express.static('client'));
-  const path = require('path');
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','index.html'))
-  })
+    const path = require('path');
+    app.get("*",(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','index.html'))
+    })
 }
 
 const PORT = process.env.PORT||3090;
